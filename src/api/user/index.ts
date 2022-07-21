11996c8a0497c8ffb2025d76ced44c3cf42be6ba
MoMeak9 <1308994506@qq.com>
2022-07-21 15:56:39 +0800

    feat: 项目初始化&请求方法封装

---
+++ src/api/user/index.ts
@@ -0,0 +1,35 @@
+import commonRequest from "../../request";
+
+interface IReq {
+    userName: string,
+    password: string
+}
+
+interface IRes {
+    token: string,
+    userName: string,
+    userId: string
+}
+
+const api = {
+    login: "/api/user/login",
+    register: "/api/user/register",
+    getUserInfo: "/api/user/getUserInfo",
+    sendCode: "/api/user/sendCode",
+    getAllUsers: "/api/user/getAllUsers",
+};
+
+export const login = (data) => {
+    return commonRequest<IReq, IRes>({
+        url: api.login,
+        method: "post",
+        data,
+        // 需要进行的额外处理
+        interceptors: {
+            requestInterceptors: (config) => {
+                console.log('注入拦截器实例');
+                return config;
+            },
+        }
+    });
+}
